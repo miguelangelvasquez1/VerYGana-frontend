faaba7a0f9f6bfa33c81af32a49289064400400c
Miguel Vasquez <vgmiguel16@gmail.com>
2025-07-17 11:52:20 -0500

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+
+    return (
+
+        <>
+        <div className="bg-blend-hue">
+            <h1 className="text-2xl, text-amber-400">Hola como estas</h1>
+        </div>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Login from "./login/page";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,6 +15,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Login />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
